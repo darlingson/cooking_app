65cd726f52067c0abb35ea1d75f02d468ad0ce73
darlingson <dmakuwila@gmail.com>
2024-05-10 07:53:15 +0200

    created a theme file

---
+++ src/themes/theme.ts
@@ -0,0 +1,41 @@
+// themes/themes.ts
+export const lightTheme = {
+    colors: {
+      primary: '#6200ee',
+      secondary: '#03dac6',
+      background: '#ffffff',
+      text: '#000000',
+      error: '#b00020',
+    },
+    fontSizes: {
+      small: 12,
+      medium: 16,
+      large: 20,
+    },
+    spacing: {
+      small: 8,
+      medium: 16,
+      large: 24,
+    },
+  };
+  
+  export const darkTheme = {
+    colors: {
+      primary: '#bb86fc',
+      secondary: '#03dac6',
+      background: '#121212',
+      text: '#ffffff',
+      error: '#cf6679',
+    },
+    fontSizes: {
+      small: 12,
+      medium: 16,
+      large: 20,
+    },
+    spacing: {
+      small: 8,
+      medium: 16,
+      large: 24,
+    },
+  };
+  
