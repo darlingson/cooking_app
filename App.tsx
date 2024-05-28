4a5b29bf2a21efba08c2ab039242135b5994560e
darlingson <dmakuwila@gmail.com>
2024-05-28 18:48:42 +0200

    added splash icon from bing chat

--- App.tsx
+++ App.tsx
@@ -13,17 +13,49 @@ import { DefaultTheme, DarkTheme } from "@react-navigation/native";
 import  {RecipeDetailsScreen}  from "./src/screens/recipe_details";
 import Recipe from "./src/types/recipe";
 import SearchScreen from "./src/screens/search_screen";
-
+import Entypo from '@expo/vector-icons/Entypo';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import { useCallback, useEffect, useState } from "react";
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator<RootStackParamList>();
 export type RootStackParamList = {
   RecipeDetails: { recipe: Recipe };
   Tabs: undefined;
 };
+SplashScreen.preventAutoHideAsync();
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
   const scheme = useColorScheme();
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
   return (
-    <NavigationContainer theme={scheme === "dark" ? DarkTheme : DefaultTheme}>
+    <NavigationContainer theme={scheme === "dark" ? DarkTheme : DefaultTheme} onReady={onLayoutRootView}>
       <Stack.Navigator>
         <Stack.Screen name="Tabs" component={MyTabs} options={{ headerShown: false }} />
         <Stack.Screen name="RecipeDetails" component={RecipeDetailsScreen}/>
